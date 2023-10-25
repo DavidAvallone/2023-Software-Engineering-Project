f547f92c2d2351a7eb7a7916e2f3f843761540f6
DavidAvallone <70533453+DavidAvallone@users.noreply.github.com>
2023-10-25 19:07:16 -0400

    fixed the round logic

--- FutureTechPoker/src/test/java/RoundTest.java
+++ FutureTechPoker/src/test/java/RoundTest.java
@@ -91,6 +91,7 @@ public class RoundTest {
         assertEquals(true, round.getGameOver());
     }
 
+    /**
     @Test
     public void round_one_update(){
         Player p1 = new Player(0, "dave", 10000);
@@ -110,10 +111,35 @@ public class RoundTest {
         round.player_turn(p2.getId(),"check",0);
         round.player_turn(p3.getId(), "call", 0);
         round.player_turn(p4.getId(),"call", 0);
-        //System.out.println(round);
+        System.out.println(round);
         round.update_round();
         assertEquals(2,round.getRound_num());
     }
+    **/
 
+    @Test
+    public void test_is_round_over(){
+        Player p1 = new Player(0, "dave", 10000);
+        Player p2 = new Player(1, "bolden", 10000);
+        Player p3 = new Player(2, "alex", 10000);
+        Player p4 = new Player(3, "neil", 10000);
+        ArrayList<Player> players = new ArrayList<Player>();
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+        players.add(p4);
+
+        int starting_bet = 50;
+        Round round = new Round(players,starting_bet);
+        round.player_turn(p1.getId(),"call",0);
+        round.player_turn(p2.getId(),"check",0);
+        round.player_turn(p3.getId(), "call", 0);
+        round.player_turn(p4.getId(),"raise", 10);
+        // continue betting
+        round.player_turn(p1.getId(),"call",0);
+        round.player_turn(p2.getId(),"call",0);
+        round.player_turn(p3.getId(), "call", 0);
+        assertEquals(true, round.isRoundOver());
+    }
 
 }
