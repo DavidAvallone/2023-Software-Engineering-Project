c47a7be81a424db185507d02b03d81e7c225cc06
DavidAvallone <70533453+DavidAvallone@users.noreply.github.com>
2023-10-25 14:34:42 -0400

    need to figure out logic for isroundover

--- FutureTechPoker/src/test/java/RoundTest.java
+++ FutureTechPoker/src/test/java/RoundTest.java
@@ -9,7 +9,7 @@ public class RoundTest {
         Player p1 = new Player(0, "dave", 10000);
         Player p2 = new Player(1, "bolden", 10000);
         Player p3 = new Player(2, "alex", 10000);
-        Player p4 = new Player(0, "neil", 10000);
+        Player p4 = new Player(3, "neil", 10000);
         ArrayList<Player> players = new ArrayList<Player>();
         players.add(p1);
         players.add(p2);
@@ -18,7 +18,102 @@ public class RoundTest {
 
         int starting_bet = 50;
         Round round = new Round(players,starting_bet);
-        System.out.println(round);
+        //System.out.println(round);
         assertEquals(50, starting_bet);
     }
+
+    @Test
+    public void round_one_pot(){
+        Player p1 = new Player(0, "dave", 10000);
+        Player p2 = new Player(1, "bolden", 10000);
+        Player p3 = new Player(2, "alex", 10000);
+        Player p4 = new Player(3, "neil", 10000);
+        ArrayList<Player> players = new ArrayList<Player>();
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+        players.add(p4);
+
+        int starting_bet = 50;
+        Round round = new Round(players,starting_bet);
+
+        round.player_turn(p1.getId(),"call",0);
+        round.player_turn(p2.getId(),"check",0);
+        round.player_turn(p3.getId(), "call", 0);
+        round.player_turn(p4.getId(),"call", 0);
+        //System.out.println(round);
+        assertEquals(200,round.getCurrent_pot());
+    }
+    @Test
+    public void round_one_folds(){
+        Player p1 = new Player(0, "dave", 10000);
+        Player p2 = new Player(1, "bolden", 10000);
+        Player p3 = new Player(2, "alex", 10000);
+        Player p4 = new Player(3, "neil", 10000);
+        ArrayList<Player> players = new ArrayList<Player>();
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+        players.add(p4);
+
+        int starting_bet = 50;
+        Round round = new Round(players,starting_bet);
+
+        round.player_turn(p1.getId(),"call",0);
+        round.player_turn(p2.getId(),"fold",0);
+        round.player_turn(p3.getId(), "fold", 0);
+        round.player_turn(p4.getId(),"fold", 0);
+        //System.out.println(round);
+        assertEquals(100,round.getCurrent_pot());
+    }
+
+    @Test
+    public void round_one_folds_gameover(){
+        Player p1 = new Player(0, "dave", 10000);
+        Player p2 = new Player(1, "bolden", 10000);
+        Player p3 = new Player(2, "alex", 10000);
+        Player p4 = new Player(3, "neil", 10000);
+        ArrayList<Player> players = new ArrayList<Player>();
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+        players.add(p4);
+
+        int starting_bet = 50;
+        Round round = new Round(players,starting_bet);
+
+        round.player_turn(p1.getId(),"call",0);
+        round.player_turn(p2.getId(),"fold",0);
+        round.player_turn(p3.getId(), "fold", 0);
+        round.player_turn(p4.getId(),"fold", 0);
+        round.update_round();
+        //System.out.println(round);
+        assertEquals(true, round.getGameOver());
+    }
+
+    @Test
+    public void round_one_update(){
+        Player p1 = new Player(0, "dave", 10000);
+        Player p2 = new Player(1, "bolden", 10000);
+        Player p3 = new Player(2, "alex", 10000);
+        Player p4 = new Player(3, "neil", 10000);
+        ArrayList<Player> players = new ArrayList<Player>();
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+        players.add(p4);
+
+        int starting_bet = 50;
+        Round round = new Round(players,starting_bet);
+
+        round.player_turn(p1.getId(),"call",0);
+        round.player_turn(p2.getId(),"check",0);
+        round.player_turn(p3.getId(), "call", 0);
+        round.player_turn(p4.getId(),"call", 0);
+        //System.out.println(round);
+        round.update_round();
+        assertEquals(2,round.getRound_num());
+    }
+
+
 }
