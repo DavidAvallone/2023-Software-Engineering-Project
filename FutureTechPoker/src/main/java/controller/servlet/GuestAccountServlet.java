e5ea3e48d9b922228a2a76f70af9c2da349ba291
NFK718 <nellishobbes@gmail.com>
2023-11-05 16:29:50 -0500

    Add Guest Account Servlet

---
+++ FutureTechPoker/src/main/java/controller/servlet/GuestAccountServlet.java
@@ -0,0 +1,21 @@
+package controller.servlet;
+
+import controller.service.UserService;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.entity.User;
+
+import java.io.IOException;
+
+@WebServlet(name = "guestAccountServlet", value = "/guestAccountServlet")
+public class GuestAccountServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User guestLogged = User.createGuest();
+        HttpSession session = request.getSession();
+        session.setAttribute("User", guestLogged); // Adding user to session
+        response.sendRedirect("home.jsp");
+    }
+}
