1e9a6644bcd164f3d4e66255c637a4866e386559
NFK718 <nellishobbes@gmail.com>
2023-12-03 12:48:41 -0500

    Add get methods and sorting methods

--- FutureTechPoker/src/main/java/controller/service/TableManager.java
+++ FutureTechPoker/src/main/java/controller/service/TableManager.java
@@ -2,7 +2,9 @@ package controller.service;
 
 import Poker.Player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TableManager {
@@ -32,4 +34,63 @@ public class TableManager {
         RoundService roundService = new RoundService();
         tables.put(tableId, roundService);
     }
+
+    public static Map<String, RoundService> getTables(){
+        return tables;
+    }
+
+    public static String sortGames(String sortBy) {
+        Map<String, RoundService> tables = TableManager.getTables();
+        StringBuilder htmlBuilder = new StringBuilder();
+        String label = "label";
+
+        switch (sortBy){
+            case "players": label = "Players"; break;
+            case "blind": label = "Big Blind"; break;
+            case "start_bet": label = "Start Bet"; break;
+        }
+
+        final String finalLabel = label;
+
+        if (tables != null) {
+            htmlBuilder.append("<div class=\"button-container table-buttons\">");
+            tables.entrySet().stream()
+                    .sorted((e1, e2) -> Integer.compare(returnSortOptionValue(e2, sortBy), returnSortOptionValue(e1, sortBy)))
+                    .forEach(entry -> {
+                        htmlBuilder.append("<button onclick=\"location.href='m_table.jsp?n=").append(entry.getKey()).append("';\"> ").append(finalLabel).append(": ").append(returnSortOptionValue(entry, sortBy)).append("</button>");
+                    });
+            htmlBuilder.append("</div>");
+        } else {
+            return "<p>No Active Tables to Sort</p>";
+
+        }
+
+        return htmlBuilder.toString();
+    }
+
+    public static int returnSortOptionValue(Map.Entry<String, RoundService> entry, String sortOption){
+        switch (sortOption) {
+            case "blind": return (int) entry.getValue().round.getBigBlind();
+            case "start_bet": return (int) entry.getValue().round.getStarting_bet();
+            default: return entry.getValue().round.getPlayers().size();
+        }
+    }
+
+    public static String getMostFilledTable() {
+        Map<String, RoundService> tables = TableManager.getTables();
+        String mostFilledTableLink = "window.location.href='home.jsp'"; // Default link
+        int maxPlayers = 0;
+
+        for (Map.Entry<String, RoundService> entry : tables.entrySet()) {
+            RoundService roundService = entry.getValue();
+            int playerCount = roundService.round.getPlayers().size();
+
+            if (playerCount > maxPlayers) {
+                maxPlayers = playerCount;
+                mostFilledTableLink = "window.location.href='m_table.jsp?n=" + entry.getKey() + "'";
+            }
+        }
+        return mostFilledTableLink;
+    }
+
 }
